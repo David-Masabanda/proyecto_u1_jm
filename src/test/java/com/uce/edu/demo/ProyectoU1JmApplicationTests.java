8864f72bc62f1341adffa0fb3f2e7d4258f7bd3a
David-Masabanda <josue.masabanda99@gmail.com>
2022-05-27 20:07:13 -0500

    Mi primera subida desde consola

---
+++ src/test/java/com/uce/edu/demo/ProyectoU1JmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.edu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoU1JmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
